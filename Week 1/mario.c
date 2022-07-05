0b3a1499a1a7035240311ea60434b916f1ab91cb
Raul <rauljlle@gmail.com>
2022-07-05 12:45:17 -0300

    initial commit

---
+++ Week 1/mario.c
@@ -0,0 +1,50 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int askForHeight(void);
+void printBricks(int n);
+
+int main(void)
+{
+    int height = askForHeight();
+    printBricks(height);
+}
+
+
+void printBricks(int n)
+{
+    //For i in n
+    for
+    (int i = 1; i <= n; i++)
+    {
+        //print (n - i) spaces,
+        for
+        (int j = n - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+        //print i times '#',
+        for
+        (int k = i; k > 0; k--)
+        {
+            printf("#");
+        }
+        //and break the line
+        printf("\n");
+    }
+}
+
+
+int askForHeight(void)
+{
+    int n;
+    // While height is not valid, keep
+    // asking for a valid height
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n <= 0 || n > 8);
+
+    return n;
+}
